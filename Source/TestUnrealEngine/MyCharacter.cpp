e5ae74cbb1690d3696bb250b7aa762d7899b84f2
hayoonlee <htdobad@gmail.com>
2022-07-18 02:24:03 +0900

    언리얼 엔진 기초 - 캐릭터 생성

---
+++ Source/TestUnrealEngine/MyCharacter.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPRINGARM"));
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+
+	// Character 클래스의 RootComponent는 캡슐 컴포넌트이므로 SpringArm을 캡슐 컴포넌트에 붙인다.
+	SpringArm->SetupAttachment(GetCapsuleComponent());
+	Camera->SetupAttachment(SpringArm);
+
+	// 스프링암의 길이, 회전 조정
+	SpringArm->TargetArmLength = 500.f;
+	SpringArm->SetRelativeRotation(FRotator(-35.f, 0.f, 0.f));
+
+	// 캐릭터의 초기 위치, 회전 조정
+	GetMesh()->SetRelativeLocationAndRotation(
+		FVector(0.f, 0.f, -88.f), FRotator(0.f, -90.f, 0.f));
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SM(TEXT("SkeletalMesh'/Game/ParagonGreystone/Characters/Heroes/Greystone/Meshes/Greystone.Greystone'"));
+	if (SM.Succeeded())
+	{
+		GetMesh()->SetSkeletalMesh(SM.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("UpDown"), this, &AMyCharacter::UpDown);
+	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AMyCharacter::LeftRight);
+	PlayerInputComponent->BindAxis(TEXT("Yaw"), this, &AMyCharacter::Yaw);
+}
+
+void AMyCharacter::UpDown(float Value)
+{
+	if (Value == 0.f)
+		return;
+
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void AMyCharacter::LeftRight(float Value)
+{
+	if (Value == 0.f)
+		return;
+
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void AMyCharacter::Yaw(float Value)
+{
+	AddControllerYawInput(Value);
+}
