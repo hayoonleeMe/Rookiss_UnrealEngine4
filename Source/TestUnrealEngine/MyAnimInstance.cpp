842608779fd7734516df7c9bea3917fd630e8ebf
hayoonlee <htdobad@gmail.com>
2022-07-18 18:21:56 +0900

    애니메이션 - 애니메이션 몽타주

--- Source/TestUnrealEngine/MyAnimInstance.cpp
+++ Source/TestUnrealEngine/MyAnimInstance.cpp
@@ -5,6 +5,15 @@
 #include "GameFramework/Character.h"
 #include "GameFramework/PawnMovementComponent.h"
 
+UMyAnimInstance::UMyAnimInstance()
+{
+	static ConstructorHelpers::FObjectFinder<UAnimMontage> AM(TEXT("AnimMontage'/Game/Animations/Greystone_Skeleton_Montage.Greystone_Skeleton_Montage'"));
+	if (AM.Succeeded())
+	{
+		AttackMontage = AM.Object;
+	}
+}
+
 // 한 프레임의 호출순서상 엔진이 입력시스템을 먼저 호출한 다음에 콘텐츠 로직을 다 실행하고, 그 다음에 최종적으로 AnimInstance의 이런 업데이트 함수를 호출한다.
 void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
@@ -22,3 +31,11 @@ void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 		}
 	}
 }	
+
+void UMyAnimInstance::PlayAttackMontage()
+{
+	if (!Montage_IsPlaying(AttackMontage))
+	{
+		Montage_Play(AttackMontage, 1.f);
+	}
+}
